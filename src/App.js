0d0b9eba84f7f83c111cf9158f6d0156bc4d0281
Chandan <chandant55@gmail.com>
2023-08-15 17:31:55 +0530

    Changes to Password app-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import usePasswordGenerator from "./hooks/use-password-generator";
+import "./index.css";
+import PasswordStrengthIndicator from "./components/strengthchecker";
+import Button from "./Button";
+import CheckBox from "./components/Checkbox";
 
 function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false },
+  ]);
+
+  const [copied, setCopied] = useState(false);
+
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* Password Text and Copy */}
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+
+          <Button
+            text={copied ? "copied" : "copy"}
+            customClass={"copyBtn"}
+            onClick={handleCopy}
+          />
+        </div>
+      )}
+      {/* Character length */}
+      <div className="charLength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+      {/* checkboxes */}
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <CheckBox
+              key={index}
+              title={checkbox.title}
+              state={checkbox.state}
+              onChange={() => handleCheckboxChange(index)}
+            />
+          );
+        })}
+      </div>
+
+      {/* strength */}
+
+      <PasswordStrengthIndicator password={password} />
+
+      {/* error handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+      {/* generate button */}
+
+      <Button
+        text="Generate Password"
+        onClick={() => {
+          generatePassword(checkboxData, length);
+        }}
+        customClass="generateBtn"
+      />
     </div>
   );
 }
